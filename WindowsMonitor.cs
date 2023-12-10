9694f828a1f81777bc545c681a6af0b4f1c21b6c
Corpella <alessandrocordella97@gmail.com>
2023-12-10 21:23:05 +0100

    ✨ feat: export method to native AOT

--- WindowsMonitor.cs
+++ WindowsMonitor.cs
@@ -1,23 +1,68 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using LibreHardwareMonitor.Hardware;
 
-public class HardwareInfo
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+public static class HardwareInfo
 {
-    private static Computer _computer;
+    private static Computer _computer = new Computer();
 
-    public HardwareInfo()
-    {
-        _computer = new Computer(); // Initialize the `_computer` field.
-    }
-
-    public void Open()
+    public static void OpenAndClose(Action action)
     {
         _computer.Open();
+        try
+        {
+            action();
+        }
+        finally
+        {
+            _computer.Close();
+        }
     }
 
-    public void Close()
+    // Modify the method signature in HardwareInfo.cs
+    [UnmanagedCallersOnly(EntryPoint = "GetHardwareInfo")]
+    public static IntPtr GetHardwareInfo(IntPtr hardwareNamesPtr)
     {
-        _computer.Close();
+        // Convert the IntPtr to an array of strings
+        string[] hardwareNames = Marshal
+            .PtrToStringAnsi(hardwareNamesPtr)
+            .Split('\0', StringSplitOptions.RemoveEmptyEntries);
+
+        var result = new Dictionary<string, HardwareInfoObject>();
+
+        OpenAndClose(() =>
+        {
+            foreach (IHardware hardware in _computer.Hardware)
+            {
+                if (hardwareNames.Contains(hardware.Name))
+                {
+                    HardwareInfoObject hardwareInfoObject = new HardwareInfoObject();
+                    hardwareInfoObject.Name = hardware.Name.Contains("Gpu") ? "Gpu" : hardware.Name;
+                    hardwareInfoObject.HardwareType = hardware.HardwareType;
+
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        SensorInfoObject sensorInfoObject = new SensorInfoObject();
+                        sensorInfoObject.Name = sensor.Name;
+                        sensorInfoObject.Value = (float)sensor.Value;
+
+                        hardwareInfoObject.Sensors.Add(sensorInfoObject);
+                    }
+
+                    result.Add(hardware.Name, hardwareInfoObject);
+                }
+            }
+        });
+
+        // Convert the result to a stringified JSON using System.Text.Json
+        string jsonResult = System.Text.Json.JsonSerializer.Serialize(result);
+        IntPtr ptr = Marshal.StringToHGlobalAnsi(jsonResult);
+
+        return ptr;
     }
 
     public class HardwareInfoObject
@@ -38,34 +83,5 @@ public class HardwareInfo
         public float Value { get; set; }
 
         public SensorInfoObject() { }
-    }
-
-    public static Dictionary<string, HardwareInfoObject> GetHardwareInfo(string[] hardwareNames)
-    {
-        Dictionary<string, HardwareInfoObject> hardwareInfoObjects =
-            new Dictionary<string, HardwareInfoObject>();
-
-        foreach (IHardware hardware in _computer.Hardware)
-        {
-            if (hardwareNames.Contains(hardware.Name))
-            {
-                HardwareInfoObject hardwareInfoObject = new HardwareInfoObject();
-                hardwareInfoObject.Name = hardware.Name.Contains("Gpu") ? "Gpu" : hardware.Name;
-                hardwareInfoObject.HardwareType = hardware.HardwareType;
-
-                foreach (ISensor sensor in hardware.Sensors)
-                {
-                    SensorInfoObject sensorInfoObject = new SensorInfoObject();
-                    sensorInfoObject.Name = sensor.Name;
-                    sensorInfoObject.Value = (float)sensor.Value;
-
-                    hardwareInfoObject.Sensors.Add(sensorInfoObject);
-                }
-
-                hardwareInfoObjects.Add(hardware.Name, hardwareInfoObject);
-            }
-        }
-
-        return hardwareInfoObjects;
     }
 }
