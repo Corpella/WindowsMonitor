d4e96596b838cb2f93984d368d09bb0ae5bb4a47
Corpella <alessandrocordella97@gmail.com>
2024-01-13 22:53:45 +0100

    🔎 ref: write code more suitable for AOT, implement basic unit testing

--- WindowsMonitor.cs
+++ WindowsMonitor.cs
@@ -1,65 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
 using LibreHardwareMonitor.Hardware;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 public static class HardwareInfo
 {
-    private static Computer _computer = new Computer();
+    private static readonly Computer _computer = new()
+    {
+        IsCpuEnabled = true,
+        IsGpuEnabled = true,
+        IsMemoryEnabled = true,
+        IsMotherboardEnabled = true,
+        IsControllerEnabled = true,
+        IsNetworkEnabled = true,
+        IsStorageEnabled = true
+    };
+    private static readonly UpdateVisitor _visitor = new();
 
-    public static void OpenAndClose(Action action)
+    static HardwareInfo()
     {
         _computer.Open();
-        try
-        {
-            action();
-        }
-        finally
-        {
-            _computer.Close();
-        }
+        _computer.Accept(_visitor);
     }
 
-    // Modify the method signature in HardwareInfo.cs
-    [UnmanagedCallersOnly(EntryPoint = "GetHardwareInfo")]
+    // FIXME: remember to remove the comment on the following line when compiling natively
+    // [UnmanagedCallersOnly(EntryPoint = "GetHardwareInfo")]
     public static IntPtr GetHardwareInfo(IntPtr hardwareNamesPtr)
     {
         // Convert the IntPtr to an array of strings
         string[] hardwareNames = Marshal
             .PtrToStringAnsi(hardwareNamesPtr)
-            .Split('\0', StringSplitOptions.RemoveEmptyEntries);
+            .Split(',', StringSplitOptions.RemoveEmptyEntries);
 
         var result = new Dictionary<string, HardwareInfoObject>();
 
-        OpenAndClose(() =>
+
+        foreach (IHardware hardware in _computer.Hardware)
         {
-            foreach (IHardware hardware in _computer.Hardware)
+            string hardwareType = hardware.HardwareType.ToString();
+            if (hardwareNames.Contains(hardwareType))
             {
-                if (hardwareNames.Contains(hardware.Name))
-                {
-                    HardwareInfoObject hardwareInfoObject = new HardwareInfoObject();
-                    hardwareInfoObject.Name = hardware.Name.Contains("Gpu") ? "Gpu" : hardware.Name;
-                    hardwareInfoObject.HardwareType = hardware.HardwareType;
-
-                    foreach (ISensor sensor in hardware.Sensors)
+                HardwareInfoObject hardwareInfoObject =
+                    new()
                     {
-                        SensorInfoObject sensorInfoObject = new SensorInfoObject();
-                        sensorInfoObject.Name = sensor.Name;
-                        sensorInfoObject.Value = (float)sensor.Value;
+                        Name = hardwareType.Contains("Gpu") ? "Gpu" : hardwareType,
+                        HardwareType = hardware.HardwareType
+                    };
 
-                        hardwareInfoObject.Sensors.Add(sensorInfoObject);
-                    }
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    float SensorValue = sensor.Value != null ? (float)sensor.Value : 0;
+                    SensorInfoObject sensorInfoObject =
+                        new() { Name = sensor.Name, Value = SensorValue, Type = hardwareType };
 
-                    result.Add(hardware.Name, hardwareInfoObject);
+                    hardwareInfoObject.Sensors.Add(sensorInfoObject);
                 }
+
+                result.Add(hardware.Name, hardwareInfoObject);
             }
-        });
+        }
+
 
         // Convert the result to a stringified JSON using System.Text.Json
-        string jsonResult = System.Text.Json.JsonSerializer.Serialize(result);
+        string jsonResult = System.Text.Json.JsonSerializer.Serialize(result,new JsonSerializerOptions { NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowNamedFloatingPointLiterals });
         IntPtr ptr = Marshal.StringToHGlobalAnsi(jsonResult);
 
         return ptr;
@@ -68,7 +73,7 @@ public static class HardwareInfo
     public class HardwareInfoObject
     {
         public string Name { get; set; }
-        public LibreHardwareMonitor.Hardware.HardwareType HardwareType { get; set; }
+        public HardwareType HardwareType { get; set; }
         public List<SensorInfoObject> Sensors { get; set; }
 
         public HardwareInfoObject()
@@ -80,8 +85,68 @@ public static class HardwareInfo
     public class SensorInfoObject
     {
         public string Name { get; set; }
+        public string Type { get; set; }
         public float Value { get; set; }
 
         public SensorInfoObject() { }
+    }
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+}
+
+[TestClass]
+public class HardwareInfoTest
+{
+    [TestMethod]
+    public void TestGetHardwareInfo()
+    {
+        // Arrange
+        string[] hardwareNames = new string[]
+        {
+            "Cpu",
+            "NvidiaGpu",
+            "Motherboard",
+            "Memory",
+            "Psu"
+         };
+
+        IntPtr hardwareNamesPtr = Marshal.StringToHGlobalAnsi(string.Join(",", hardwareNames));
+
+        // Act
+        IntPtr resultPtr = HardwareInfo.GetHardwareInfo(hardwareNamesPtr);
+
+        // Convert the result back to a string
+        string jsonResult = Marshal.PtrToStringAnsi(resultPtr);
+
+        // Log the result
+        Console.WriteLine(jsonResult);
+
+        // Convert the JSON string to a Dictionary
+        var result = System.Text.Json.JsonSerializer.Deserialize<
+            Dictionary<string, HardwareInfo.HardwareInfoObject>
+        >(jsonResult);
+
+        // Assert
+        Assert.AreEqual(hardwareNames.Length, result.Count);
+        foreach (string hardwareName in hardwareNames)
+        {
+            Assert.IsTrue(result.ContainsKey(hardwareName));
+        }
+
+        // Clean up
+        Marshal.FreeHGlobal(hardwareNamesPtr);
+        Marshal.FreeHGlobal(resultPtr);
     }
 }
